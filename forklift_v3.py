9b283286280ef4c6d514c33d4d6f54a324469aba
Artem <jojoba37544@yandex.ru>
2020-11-20 17:09:49 +0300

    ALMOST WORKS

--- forklift_v3.py
+++ forklift_v3.py
@@ -955,8 +955,6 @@ material_name_dict = {
 
 }
 
-path = 'lx03.txt'
-
 
 def default_dict_creation(list_name):
     default_dict = defaultdict(list)
@@ -965,33 +963,9 @@ def default_dict_creation(list_name):
     return dict(default_dict)
 
 
-def tuple_to_int(tuple):
-    result = int(''.join((str(tuple[0]), f'{(tuple[1]):0>5}')))
+def tuple_to_int(tup):
+    result = int(''.join((str(tup[0]), f'{(tup[1]):0>5}')))
     return result
-
-
-def chat(string):
-    while True:
-        try:
-            answer = int(input(string))
-        except ValueError:
-            continue
-        return answer
-
-def conditions(eq_1, eq_2):
-    condition_list = [(eq_1 == eq_2),
-                      (eq_1 == eq_2 + 1),
-                      (eq_1 == eq_2 - 1),
-                      (eq_1 == eq_2 + 2),
-                      (eq_1 == eq_2 - 2),
-                      ]
-    return condition_list
-
-
-# def date_harmonizer(x, test_list):
-#     result = 0
-#     if
-#     return result
 
 
 class BinNotFound(Exception):
@@ -1023,62 +997,58 @@ class MapCreator:
     def _look_for_items(self, line):
         material = line[2]
         bin_pos = line[5]
-        date = line[6][:-1]
+        dat = line[6][:-1]
         quantity = line[4].split(' ')[-1:][0]  # splits and returns the last elem as member of list
-        date_reversed = int(''.join((date[-4:], date[-7:-5], date[-10:-8])))  # as the only member so [0]
+        date_reversed = int(''.join((dat[-4:], dat[-7:-5], dat[-10:-8])))  # as the only member so [0]
         if quantity == '0':
             return
         self.container.append((material, (bin_pos, (quantity, date_reversed))))
 
+    def _dictionary(self):
+        self.dict = default_dict_creation(self.container)
+        for material_code, value in self.dict.items():
+            date_set = sorted(list({x for x in value}))
+            for dat in date_set:
+                cprint(date_set, 'blue')
+                for index_y, y in enumerate(date_set):
+                    if y == dat:
+                        continue
+                    if (y[0], y[1][1]) == (dat[0], dat[1][1] + 1):
+                        date_set.remove(y)
+                        print(date_set)
+                    elif y[1][1] == dat[1][1] + 1:
+                        date_set[index_y] = (y[0], (y[1][0], dat[1][1]))
+                        cprint(date_set, 'red')
+            for dat in date_set:
+                condition_1 = (dat[0], (dat[1][0], dat[1][1] + 1))
+                condition_2 = (dat[0], (dat[1][0], dat[1][1] - 1))
+                for index, item in enumerate(value):
+                    if item == dat or item == condition_1 or item == condition_2:
+                        value[index] = dat
+        for material_code, value in self.dict.items():
+            self.dict[material_code] = default_dict_creation(value)
+
+    def data_return(self):
+        self._dictionary()
+        for material_code, bin_name in self.dict.items():
+            for bin_name_l, value in bin_name.items():
+                date_set = {x[1] for x in value}
+                print(date_set)
+                bin_name[bin_name_l] = []
+                for dat in date_set:
+                    summ = sum([int(x[0]) for x in value if x[1] == dat])
+                    # bin_name[bin_name_l].append((summ, dat))
+                    bin_name[bin_name_l] = tuple_to_int((dat, summ))
+        return self.dict
+
 
 fork_map = MapCreator('lx02.txt')
 
-# pprint(fork_map.container)
 
-mat_dict = default_dict_creation(fork_map.container)
-date_dict = {}
-
-
-for material_code, value in mat_dict.items():
-    date_set = sorted(list({x for x in value}))
-    for date in date_set:
-        cprint(date_set, 'blue')
-        for index_y, y in enumerate(date_set):
-            if y == date:
-                continue
-            if (y[0], y[1][1]) == (date[0], date[1][1] + 1):
-                date_set.remove(y)
-                print(date_set)
-            elif y[1][1] == date[1][1] + 1:
-                date_set[index_y] = (y[0], (y[1][0], date[1][1]))
-                cprint(date_set, 'red')
-
-    for date in date_set:
-        condition_1 = (date[0], (date[1][0], date[1][1] + 1))
-        condition_2 = (date[0], (date[1][0], date[1][1] - 1))
-        for index, item in enumerate(value):
-            if item == date or item == condition_1 or item == condition_2:
-                value[index] = date
-
-for material_code, value in mat_dict.items():
-    mat_dict[material_code] = default_dict_creation(value)
-
-
-for material_code, bin_name in mat_dict.items():
-    for bin_name_l, value in bin_name.items():
-        date_set = {x[1] for x in value}
-        print(date_set)
-        bin_name[bin_name_l] = []
-        for date in date_set:
-            summ = sum([int(x[0]) for x in value if x[1] == date])
-            bin_name[bin_name_l].append((summ, date))
-
-pprint(mat_dict)
-
-for material_code, bin_name in mat_dict.items():
-    for bin_name_l, value in bin_name.items():
-        if len(value) >= 2:
-            print(material_code, bin_name)
+for material_code, bin_name in fork_map.data_return().items():
+    print(material_code, bin_name)
+    print(" "*8, ['.'.join((str(x)[8:],  str(x)[6:8], str(x)[4:6], str(x)[:4])) for x in bin_name.values()])
+    cprint(f'{(min(bin_name, key=bin_name.get))}', 'red')
 
 
 
