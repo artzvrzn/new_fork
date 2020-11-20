be07f7c9b2f3a672c922a5ad49d09da946ca354d
Artem <jojoba37544@yandex.ru>
2020-11-21 01:58:46 +0300

    log file added

--- forklift_v3.py
+++ forklift_v3.py
@@ -53,27 +53,33 @@ class MapCreator:
         material = line[2]
         bin_pos = line[5]
         dat = line[6][:-1]
-        quantity = line[4].split(' ')[-1:][0]  # splits and returns the last elem as member of list
+        bin_quantity = line[4].split(' ')[-1:][0]  # splits and returns the last elem as member of list
         date_reversed = int(''.join((dat[-4:], dat[-7:-5], dat[-10:-8])))  # as the only member so [0]
-        if quantity == '0':
+        if bin_quantity == '0':
             return
-        self.container.append((material, (bin_pos, (quantity, date_reversed))))
+        self.container.append((material, (bin_pos, (bin_quantity, date_reversed))))
 
     def _dictionary(self):
         self.dict = default_dict_creation(self.container)
+        log_file = open('log_file.txt', 'w', encoding='utf8')
         for material_code, value in self.dict.items():
-            date_set = sorted(list({x for x in value}))
+            date_set = sorted(list({x for x in value}))  # without sorted func algorithm would work wrong
+            # TODO resolve transition from month to month in bins
+            log_file.write(f'{material_code:_^18}\n')
             for dat in date_set:
-                cprint(date_set, 'blue')
+                log_file.write(f'Check {dat[1][1]} in {date_set}\n')
                 for index_y, y in enumerate(date_set):
                     if y == dat:
                         continue
                     if (y[0], y[1][1]) == (dat[0], dat[1][1] + 1):
                         date_set.remove(y)
-                        # print(date_set)
+                        log_file.write(f'{"Removed":18}[{y}]\n{"":18}')
+                        log_file.write(f'{date_set}\n')
                     elif y[1][1] == dat[1][1] + 1:
                         date_set[index_y] = (y[0], (y[1][0], dat[1][1]))
                         cprint(date_set, 'red')
+                        log_file.write(f'{"Changed":18}[{y}]\n{"":18}')
+                        log_file.write(f'{date_set}\n')
             for dat in date_set:
                 condition_1 = (dat[0], (dat[1][0], dat[1][1] + 1))
                 condition_2 = (dat[0], (dat[1][0], dat[1][1] - 1))
@@ -82,13 +88,13 @@ class MapCreator:
                         value[index] = dat
         for material_code, bin_name in self.dict.items():
             self.dict[material_code] = default_dict_creation(bin_name)
+        log_file.close()
 
     def data_return(self):
         self._dictionary()
         for material_code, bin_name in self.dict.items():
             for bin_name_l, value in bin_name.items():
                 date_set = {x[1] for x in value}
-                # print(date_set)
                 bin_name[bin_name_l] = []
                 for dat in date_set:
                     summ = sum([int(x[0]) for x in value if x[1] == dat])
@@ -101,10 +107,10 @@ fork_map = MapCreator('lx02.txt')
 
 
 with open('map.txt', 'w', encoding='utf8') as map_file:
-    for key, value in fork_map.data_return().items():
-        print(key, [f"{x} ({interpreter(str(value.get(x)), 'reverse')})" for x in value])
-        result = f'\n{(min(value, key=value.get))}'
-        dat_quantity = str(value.get(result[1:]))
+    for key, bin_value in fork_map.data_return().items():
+        print(key, [f"{x} ({interpreter(str(bin_value.get(x)), 'reverse')})" for x in bin_value])
+        result = f'\n{(min(bin_value, key=bin_value.get))}'
+        dat_quantity = str(bin_value.get(result[1:]))
         dat_interpretation = interpreter(dat_quantity)
         quantity = interpreter(dat_quantity, 'quant')
         cprint(f'{result[1:]} {quantity} {dat_interpretation}', 'red')
